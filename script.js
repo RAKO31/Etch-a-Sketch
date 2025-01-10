7dd18e2c2d3ee23a61f8827283d3afc5cec0c8e7
RAKO31 <raghavkohli668@gmail.com>
2025-01-11 03:04:27 +0530

    make column rgb

---
+++ script.js
@@ -0,0 +1,30 @@
+let container = document.querySelector('div');
+let btn = document.createElement('button');
+btn.textContent = "Set Grid Size";
+btn.style= "margin:3px" ;
+btn.addEventListener('click',()=>{
+    let size =prompt("what size you want");
+    if(size>100 || isNaN(size)){
+        alert("invalid ")
+    }
+    else{
+        container.innerHTML='';
+        container.appendChild(btn);
+        for(let i=0;i<=size;i++){
+            let row = document.createElement('div');
+            row.className="row";
+            container.appendChild(row);
+            row.style = "display:flex;border:0.5px solid black ; width:520px";
+            for(let j=0;j<=size;j++){
+                let column = document.createElement('div');
+                column.className = `column${i+1}${j+1}`;
+                column.style=" border:1px solid black;width:10px;height:10px;flex:1";
+                column.addEventListener('mouseenter',()=>{
+                    column.style.backgroundColor = 'blue';
+                });
+                row.appendChild(column);
+            }
+        }
+    }
+});
+container.appendChild(btn);
